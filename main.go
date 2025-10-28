85c9790d5575b8492c0e0236c8ca4ca1bd550621
Daniel Kennedy <danielrkennedy03@gmail.com>
2025-10-28 15:27:43 +0000

    feat(crossover): single point crossover

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"math/rand"
 	"fmt"
-	"unsafe"
 )
 
 func MakePopulation(size, gene_length int) [][]bool {
@@ -17,8 +16,38 @@ func MakePopulation(size, gene_length int) [][]bool {
 	return population
 }
 
-func main() {
-	population := MakePopulation(100, 8)
+func GetFitness(individual []bool) float64 {
+	length := len(individual)
+	weight := 0
+	for i := range(length) {
+		if individual[i] { weight++ }
+	}
+	return float64(weight) / float64(length)
+}
 
-	fmt.Println(population)
+func SinglePointCrossover(a, b []bool) ([]bool, []bool) {
+	half := len(a) / 2
+	t := make([]bool, half)
+	copy(t, a[half:])
+	copy(a[half:], b[half:])
+	copy(b[half:], t)
+	return a, b
+}
+
+func main() {
+	//population := MakePopulation(100, 8)
+
+	//fmt.Println(population)
+
+	//for i := range(len(population)) {
+	//	fmt.Println(GetFitness(population[i]))
+	//}
+
+	a := []bool{true, true}
+	b := []bool{false, false}
+
+	a, b = SinglePointCrossover(a, b)
+
+	fmt.Println(a)
+	fmt.Println(b)
 }
