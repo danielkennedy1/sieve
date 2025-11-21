b86551a04302bbc61dd52db5cedd89de631a42be
Dominick Stephens <114415058+Dominickstephens@users.noreply.github.com>
2025-11-21 09:25:14 +0000

    Comment out grammar package import

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 	//"github.com/danielkennedy1/sieve/ea"
 	//"github.com/danielkennedy1/sieve/genomes"
-	"github.com/danielkennedy1/sieve/problems/grammar"
+	//"github.com/danielkennedy1/sieve/problems/grammar"
 	//"github.com/danielkennedy1/sieve/problems/grammar"
 )
 
