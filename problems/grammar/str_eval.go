a638e9db1f87113357859ca017c5f737805e4131
Dominick Stephens <dominickdstephens@gmail.com>
2025-11-29 18:17:04 +0000

    fix(cmt): formatting

--- problems/grammar/str_eval.go
+++ problems/grammar/str_eval.go
@@ -13,22 +13,16 @@ type Sample struct {
 }
 
 func NewRMSE(samples []Sample, gr genomes.Grammar) func(g genomes.Genotype) float64 {
-	// Define the penalty coefficient (lambda)
-	// This value controls how harshly the EA is punished for complexity.
-	// Start with a small value like 0.001 and adjust if needed.
 	const parsimonyPenalty = 0.001
 
 	return func(g genomes.Genotype) float64 {
 		varMap := genomes.BuildVarMapFromGrammar(gr)
 		exprStr := g.MapToGrammar(gr, 100).String()
 
-		// 1. Calculate Length Penalty (Complexity Component)
-		// We use the number of characters in the resulting expression string as a proxy for complexity.
 		lengthPenalty := float64(len(exprStr)) * parsimonyPenalty
 
 		program, err := expr.Compile(exprStr, expr.AllowUndefinedVariables())
 		if err != nil {
-			// fmt.Println("Failed to compile")
 			return math.Inf(-1)
 		}
 
@@ -45,7 +39,6 @@ func NewRMSE(samples []Sample, gr genomes.Grammar) func(g genomes.Genotype) floa
 				return math.Inf(-1)
 			}
 			if err != nil {
-				// fmt.Println("Failed to run")
 				return math.Inf(-1)
 			}
 
@@ -53,13 +46,8 @@ func NewRMSE(samples []Sample, gr genomes.Grammar) func(g genomes.Genotype) floa
 			total += diff * diff
 		}
 
-		// 2. Calculate RMSE (Accuracy Component)
 		rmse := math.Sqrt(total / float64(len(samples)))
 
-		// 3. Combine Fitness:
-		// Fitness = -RMSE - LengthPenalty
-		// Since we are maximizing fitness, a lower RMSE is better (more positive),
-		// and a shorter length is better (less penalty subtracted).
 		return -rmse - lengthPenalty
 	}
 }
