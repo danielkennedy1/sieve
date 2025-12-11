e8578cd3b8850beb5948d735af858ee68b1cd7e4
Dominick Stephens <dominickdstephens@gmail.com>
2025-12-11 17:30:09 +0000

    fix(flags): moved flags into main

--- main.go
+++ main.go
@@ -8,33 +8,32 @@ import (
 	"github.com/danielkennedy1/sieve/benchmark"
 )
 
-var (
-	runGA      = flag.Bool("ga", false, "Run genetic algorithm")
-	makeChart  = flag.Bool("chart", false, "Generate charts from existing data")
-	runCompare = flag.Bool("compare", true, "Run comparison between strategies")
-	dataFile   = flag.String("data", "market_history.json", "Path to market history JSON file")
-	outputDir  = flag.String("output", "charts", "Directory for chart output")
-)
-
 func main() {
+
+	runGA := flag.Bool("ga", false, "Run genetic algorithm")
+	makeChart := flag.Bool("chart", false, "Generate charts from existing data")
+	runCompare := flag.Bool("compare", false, "Run comparison between strategies")
+	dataFile := flag.String("data", "market_history.json", "Path to market history JSON file")
+	outputDir := flag.String("output", "charts", "Directory for chart output")
+
 	flag.Parse()
 
-	if *makeChart {
+	switch {
+	case *makeChart:
 		if err := benchmark.GenerateCharts(*dataFile, *outputDir); err != nil {
 			fmt.Printf("Error generating charts: %v\n", err)
 			os.Exit(1)
 		}
 		return
-	}
 
-	if *runGA {
+	case *runGA:
 		runGeneticAlgorithm()
 		return
-	}
 
-	if *runCompare {
+	case *runCompare:
 		benchmark.RunComparison()
 		return
 	}
 
+	fmt.Println("No action specified. Use -ga to run genetic algorithm, -chart to generate charts, or -compare to run comparison.")
 }
